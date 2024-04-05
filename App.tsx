f1ca3d530fa0dcc9512331d42318ab90204c05d4
zephyR00S <sayaksaha128@gmail.com>
2024-04-05 22:09:27 +0530

    Password Created

--- App.tsx
+++ App.tsx
@@ -10,13 +10,13 @@ import * as Yup from 'yup'
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
   .min(4, 'Pasword should be minimum of 4 characters')
-  .max(16, 'Password should be maximum of 15 characters')
+  .max(15, 'Password should be maximum of 15 characters')
   .required('Length is required')
   
 })
 
 export default function App() {
-
+//1st is variable and 2nd is a method
   const [password, setPassword] = useState('')
   const [isPassGenerated, setIsPassGenerated] = useState(false)
 
@@ -24,6 +24,25 @@ export default function App() {
   const [upperCase, setupperCase] = useState(false)
   const [numbers, setNumbers] = useState(false)
   const [symbols, setSymbols] = useState(false)
+
+  //generate password string
+  const generatePassword = (passwordLength: number) => {
+
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random()*characters.length)
+      result += characters.charAt(characterIndex)
+    }
+    return result
+  }
+
+  //resetting the state of the password
+  const resetPassword = () => {
+
+  }
 
   return (
     <View>
